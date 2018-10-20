dfc441d4827ca77da16a461bd89729c290d2fdaf
jxlwqq <jxlwqq@gmail.com>
2018-10-20 19:13:33 +0800

    [refactor]test

--- test_validator.py
+++ test_validator.py
@@ -8,14 +8,14 @@ from id_validator import validator
 
 class ValidatorTest(unittest.TestCase):
     def test_is_valid(self):
-        self.assertEquals(validator.is_valid('440308199901101512'), True)
-        self.assertEquals(validator.is_valid('610104620927690'), True)
-        self.assertEquals(validator.is_valid('810000199408230021'), True)
-        self.assertEquals(validator.is_valid('830000199201300022'), True)
-        self.assertEquals(validator.is_valid('440308199901101513'), False)
+        self.assertTrue(validator.is_valid('440308199901101512'))
+        self.assertTrue(validator.is_valid('610104620927690'))
+        self.assertTrue(validator.is_valid('810000199408230021'))
+        self.assertTrue(validator.is_valid('830000199201300022'))
+        self.assertFalse(validator.is_valid('440308199901101513'))
 
     def test_get_info(self):
-        self.assertEquals(validator.get_info('440308199901101512'), {
+        self.assertEqual(validator.get_info('440308199901101512'), {
             'address_code': '440308',
             'abandoned': 0,
             'address': '广东省深圳市盐田区',
@@ -28,15 +28,15 @@ class ValidatorTest(unittest.TestCase):
         })
 
     def test_fake_id(self):
-        self.assertEquals(validator.is_valid(validator.fake_id()), True)
-        self.assertEquals(validator.is_valid(validator.fake_id(False)), True)
-        self.assertEquals(validator.is_valid(validator.fake_id(True, '上海市', '2000', 1)), True)
-        self.assertEquals(validator.is_valid(validator.fake_id(True, '江苏省', '20000101', 1)), True)
-        self.assertEquals(validator.is_valid(validator.fake_id(True, '台湾省', '20131010', 0)), True)
+        self.assertTrue(validator.is_valid(validator.fake_id()))
+        self.assertTrue(validator.is_valid(validator.fake_id(False)))
+        self.assertTrue(validator.is_valid(validator.fake_id(True, '上海市', '2000', 1)))
+        self.assertTrue(validator.is_valid(validator.fake_id(True, '江苏省', '20000101', 1)))
+        self.assertTrue(validator.is_valid(validator.fake_id(True, '台湾省', '20131010', 0)))
 
     def test_upgrade_id(self):
         id_card = validator.upgrade_id('610104620927690')
-        self.assertEquals(True, validator.is_valid(id_card))
+        self.assertTrue(validator.is_valid(id_card))
 
 
 if __name__ == '__main__':
