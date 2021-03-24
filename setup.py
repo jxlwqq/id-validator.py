7e24710bec4d87ebc18f385c648981df8041f436
jxlwqq <jxlwqq@gmail.com>
2021-03-24 17:52:10 +0800

    Release 1.0.15

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='id-validator',
-      version='1.0.14',
+      version='1.0.15',
       description='Chinese Mainland Personal ID Card Validation',
       url='https://github.com/jxlwqq/id-validator.py',
       author='jxlwqq',
